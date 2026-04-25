5bdf78aae32844941f465112a9e66d542e09e179
Matheus de Souza Silva <matheus1882014@outlook.com>
2026-04-25 19:53:29 -0300

    começando a criar personagens

--- app/Personagens.cs
+++ app/Personagens.cs
@@ -1,3 +1,35 @@
 // Personagens do jogo
 
 
+public class Personagens
+{
+    public string Nome { get; set; }
+    public string Classe { get; set; }
+    public int Nivel { get; set; }
+    public int Vida { get; set; }
+    public int Forca { get; set; }
+    public int Agilidade { get; set; }
+    public int Inteligencia { get; set; }
+
+    public Personagens(string nome, string classe, int nivel, int vida, int forca, int agilidade, int inteligencia)
+    {
+        Nome = nome;
+        Classe = classe;
+        Nivel = nivel;
+        Vida = vida;
+        Forca = forca;
+        Agilidade = agilidade;
+        Inteligencia = inteligencia;
+    }
+
+    public void ExibirInformacoes()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Classe: {Classe}");
+        Console.WriteLine($"Nível: {Nivel}");
+        Console.WriteLine($"Vida: {Vida}");
+        Console.WriteLine($"Força: {Forca}");
+        Console.WriteLine($"Agilidade: {Agilidade}");
+        Console.WriteLine($"Inteligência: {Inteligencia}");
+    }
+}
