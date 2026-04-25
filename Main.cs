11fd5c66cd808f17dda5f1e61f7b20f410c7a97d
Matheus de Souza Silva <matheus1882014@outlook.com>
2026-04-25 18:30:36 -0300

    Create Main.cs

---
+++ Main.cs
@@ -0,0 +1,10 @@
+string nome = "Jogador";
+
+Console.WriteLine("Seja bem vindo ao Minecraft Zeus!");
+
+Console.WriteLine("Digite seu nome: ");
+
+nome = Console.ReadLine();
+
+Console.WriteLine($"Olá, {nome}! Seja bem vindo!");
+
